529034caabf89686d2308b53c7d1ba8a12218158
LjosNorth <hbborve@gmail.com>
2026-09-16 18:53:50 +0200

    trying to add navigation 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,27 @@ st.set_page_config(
     layout="centered",
 )
 
-st.title("lorem ipsum")
-st.write("dolor sit amet, consectetur adipiscing elit. Integer enim neque, tincidunt eu sagittis a, suscipit ac eros. Vivamus vitae porttitor lorem, id fringilla dolor. Phasellus semper eu lacus a sodales")
+home = st.Page(
+    "pages/page1.py",
+    title="home",
+    icon=""
+)
+page2 = st.Page(
+    "pages/page2.py",
+    title="page2",
+    icon="󰬻"
+)
+page3 = st.Page(
+    "pages/page3.py",
+    title="page3",
+    icon="󰬼"
+)
+settings = st.Page(
+    "pages/page4.py",
+    title="settings",
+    icon=""
+)
+
+
+# st.title("lorem ipsum")
+# st.write("dolor sit amet, consectetur adipiscing elit. Integer enim neque, tincidunt eu sagittis a, suscipit ac eros. Vivamus vitae porttitor lorem, id fringilla dolor. Phasellus semper eu lacus a sodales")
