c164251a46f130cdf55881619ff398b58a0dd776
LjosNorth <hbborve@gmail.com>
2026-09-17 14:45:13 +0200

    fixed some syntax errors for selectbox and slider

--- pages/plotData.py
+++ pages/plotData.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import streamlit as st
 from utils.reservoirsSelect import reservoirsSelect
 from utils.dataLoaders import load_Reservoirs
@@ -6,7 +7,7 @@ st.title("Nullam ac ornare tellus.")
 st.write("Duis eget sollicitudin justo. Pellentesque aliquam congue turpis sed aliquet. Etiam vitae nulla non sem elementum vulputate. ")
 
 fullReservoirs = load_Reservoirs()
-fullColumns = fullReservoirs.columns
+fullColumns = fullReservoirs.columns.to_list()
 fullColumns.append("All columns")
 
 # Select Box for Columns
@@ -14,8 +15,8 @@ optionsColumns = st.selectbox("Select what columns to display", fullColumns)
 st.write(optionsColumns)
 
 #slider
-fullReservoirs["date_Id"] = fullReservoirs["date_Id"].to_timestamp().sort_values(by="date_Id")
-minMonth = fullReservoirs["date_Id"].min().month
-maxMonth = fullReservoirs["date_Id"].max().year*12+fullReservoirs["date_Id"].max().month
+fullReservoirs["dato_Id"] = pd.to_datetime(fullReservoirs["dato_Id"]).sort_values()
+minMonth = fullReservoirs["dato_Id"].min().month
+maxMonth = fullReservoirs["dato_Id"].max().year*12+fullReservoirs["dato_Id"].max().month
 months = st.slider("Choose timelength to look at", minMonth, maxMonth)
 st.write(months)
