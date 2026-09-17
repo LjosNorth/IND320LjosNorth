6a164e1b7814c7d352f40f99b4ca1fc60b7e5d87
LjosNorth <hbborve@gmail.com>
2026-09-17 14:19:24 +0200

    structure refactor again and added slider

--- pages/plotData.py
+++ pages/plotData.py
@@ -7,6 +7,15 @@ st.write("Duis eget sollicitudin justo. Pellentesque aliquam congue turpis sed a
 
 fullReservoirs = load_Reservoirs()
 fullColumns = fullReservoirs.columns
+fullColumns.append("All columns")
 
+# Select Box for Columns
 optionsColumns = st.selectbox("Select what columns to display", fullColumns)
 st.write(optionsColumns)
+
+#slider
+fullReservoirs["date_Id"] = fullReservoirs["date_Id"].to_timestamp().sort_values(by="date_Id")
+minMonth = fullReservoirs["date_Id"].min().month
+maxMonth = fullReservoirs["date_Id"].max().year*12+fullReservoirs["date_Id"].max().month
+months = st.slider("Choose timelength to look at", minMonth, maxMonth)
+st.write(months)
