5db22c4597a90a1cbf377805a3d08f2cd16fba1e
LjosNorth <hbborve@gmail.com>
2026-09-17 13:51:57 +0200

    did some path magic and changed data_editor to dataframe im prayge

--- pages/tabularData.py
+++ pages/tabularData.py
@@ -1,10 +1,13 @@
+from pathlib import Path
 import streamlit as st
 import pandas as pd
 from utils.reservoirsSelect import reservoirsSelect
 
+ROOT = Path(__file__).resolve().parent.parent
+RESERVOIRS_FILE = ROOT / "data" / "reservoirs.csv"
 @st.cache_data
 def load_Reservoirs():
-    return pd.read_csv("data/reservoirs.csv")
+    return pd.read_csv(RESERVOIRS_FILE)
 
 st.title("Mauris lorem")
 st.write("elis, consectetur id mollis sit amet, vulputate non libero. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris eros purus, sagittis in finibus eu, ultricies sit amet nisi. Cras at sagittis eros.")
@@ -13,10 +16,10 @@ reservoirsDF = reservoirsSelect(load_Reservoirs(),columns=["fyllingsgrad", "kapa
 columns = reservoirsDF.columns
 table_data = pd.DataFrame({
     "column": columns,
-    "first month": [reservoirsDF[columns].tolist for columns in reservoirsDF.columns],
+    "first month": [reservoirsDF[columns].tolist() for columns in reservoirsDF.columns],
 })
 
-st.data_editor(
+st.dataframe(
     table_data,
     column_config={
         "name": st.column_config.TextColumn("Reservoirs"),
@@ -27,6 +30,5 @@ st.data_editor(
             y_max=100,
             help="this better work"
         )
-    },
-    disabled=True
+    }
 )
