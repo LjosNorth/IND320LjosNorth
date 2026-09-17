aeaf7bb1f0071b6a0a61c5d1fe170bfd7d9781d0
LjosNorth <hbborve@gmail.com>
2026-09-17 09:09:06 +0200

    Experiment with effect of linechartcolumn

--- pages/tabularData.py
+++ pages/tabularData.py
@@ -7,14 +7,22 @@ reservoirsDF = pd.DataFrame(reservoirs)
 st.title("Mauris lorem")
 st.write("elis, consectetur id mollis sit amet, vulputate non libero. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris eros purus, sagittis in finibus eu, ultricies sit amet nisi. Cras at sagittis eros.")
 
+st.write("with column config")
 st.data_editor(
     reservoirsDF,
     column_config={
-        "somm": st.column_config.LineChartColumn(
+        "omrnr": st.column_config.LineChartColumn(
             "filler text",
             width="medium",
             help="wtf is this my man",
-            y_min=100,
+            y_min=1,
             y_max=100
     )},
+    disabled=True
+)
+
+st.write("with column config")
+st.data_editor(
+    reservoirsDF,
+    disabled=True
 )
