fdd215d2b6fad3920c90fc37a3f3a809304853e6
LjosNorth <hbborve@gmail.com>
2026-09-17 13:40:26 +0200

    implemented a new table v.1

--- pages/tabularData.py
+++ pages/tabularData.py
@@ -1,26 +1,32 @@
 import streamlit as st
 import pandas as pd
-from utils import reservoirsSelect
+from utils.reservoirsSelect import reservoirsSelect
 
 @st.cache_data
 def load_Reservoirs():
-    return pd.read_csv("data/reservoirs.csv")
+    return pd.read_csv("../data/reservoirs.csv")
 
 st.title("Mauris lorem")
 st.write("elis, consectetur id mollis sit amet, vulputate non libero. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris eros purus, sagittis in finibus eu, ultricies sit amet nisi. Cras at sagittis eros.")
 
-reservoirsDF = reservoirsSelect.reservoirsSelect(load_Reservoirs())
+reservoirsDF = reservoirsSelect(load_Reservoirs(),columns=["fyllingsgrad", "kapasitet_TWh", "fylling_TWh"], months=1)
+columns = reservoirsDF.columns
+table_data = pd.DataFrame({
+    "column": columns,
+    "first month": [reservoirsDF[columns].tolist for columns in reservoirsDF.columns],
+})
 
-st.write("with column config")
 st.data_editor(
-    reservoirsDF["omrnr"],
+    table_data,
     column_config={
-        "omrnr": st.column_config.LineChartColumn(
-            "filler text",
+        "name": st.column_config.TextColumn("Reservoirs"),
+        "first month": st.column_config.LineChartColumn(
+            "first month",
             width="medium",
-            help="wtf is this my man",
-            y_min=1,
-            y_max=100
-    )},
+            y_min=0,
+            y_max=100,
+            help="this better work"
+        )
+    },
     disabled=True
 )
