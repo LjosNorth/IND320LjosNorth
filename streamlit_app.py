f2f9256fbbf7a70a68aa6379a57a3a869c70d248
LjosNorth <hbborve@gmail.com>
2026-09-17 14:37:42 +0200

    mighthavedoneabigfuckywucky

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,5 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="lorem ipsum",
-    page_icon="󰘁",
-    layout="centered",
-)
-
 pageHome = st.Page("pages/home.py", title="Home")
 pageTables = st.Page("pages/tabularData.py", title="Tables")
 pagePlots = st.Page("pages/plotData.py", title="Plots")
