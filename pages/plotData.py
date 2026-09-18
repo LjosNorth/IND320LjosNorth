6cd315d5bb09bb74fe1b4326fb6ed01e4a5d14d9
LjosNorth <hbborve@gmail.com>
2026-09-18 11:45:59 +0200

    plotData page restructure to class

--- pages/plotData.py
+++ pages/plotData.py
@@ -8,20 +8,9 @@ from plotly.graph_objs.layout import xaxis
 from utils.reservoirsSelect import reservoirsSelect
 from utils.dataLoaders import load_Reservoirs
 
-# settings
-pd.options.plotting.backend = "plotly"
-
-st.title("Nullam ac ornare tellus.")
-st.write("Duis eget sollicitudin justo. Pellentesque aliquam congue turpis sed aliquet. Etiam vitae nulla non sem elementum vulputate. ")
-
-fullReservoirs = load_Reservoirs()
-
 # Select Box for Columns
 # fullColumns = fullReservoirs.columns.to_list()
 # fullColumns.append("All columns")
-fullColumns=["fyllingsgrad", "kapasitet_TWh", "fylling_TWh", "fyllingsgrad_forrige_uke","endring_fyllingsgrad"] #for now reducing what can be displayed
-
-optionsColumns = st.selectbox("Select what columns to display", fullColumns)
 
 
 #slider
@@ -29,18 +18,51 @@ fullReservoirs["dato_Id"] = pd.to_datetime(fullReservoirs["dato_Id"]).sort_value
 minMonth = fullReservoirs["dato_Id"].min().month
 maxMonth = (fullReservoirs["dato_Id"].max().year-fullReservoirs["dato_Id"].min().year)*12+fullReservoirs["dato_Id"].max().month
 months = st.slider("Choose nr. of months to look at", minMonth, maxMonth)
-
+#
 #get & plotting data
-if optionsColumns == "":
-    pass
-elif optionsColumns == "All columns":
-    reservoirs = reservoirsSelect(fullReservoirs, [], months)
-else:
-    reservoirs = reservoirsSelect(fullReservoirs, [optionsColumns], months)
-    fig = go.Figure(
-        data = [go.Histogram(x=reservoirs[optionsColumns])]
-    )
-    fig.update_xaxes(title_text=optionsColumns)
-    fig.update_yaxes(title_text="count")
-    fig.show()
-    st.plotly_chart(fig)
+
+
+class PlotData:
+    pd.options.plotting.backend = "plotly" #setting, cuz matplotlib be boring
+    def __init__(self):
+        self.fullReservoirs = load_Reservoirs()
+        self.fullColumns=["fyllingsgrad", "kapasitet_TWh", "fylling_TWh", "fyllingsgrad_forrige_uke","endring_fyllingsgrad"] #for now reducing what can be displayed
+        self.fullReservoirs["dato_Id"] = pd.to_datetime(self.fullReservoirs["dato_Id"]).sort_values()
+        self.minMonth = self.fullReservoirs["dato_Id"].min().month
+        self.maxMonth = (self.fullReservoirs["dato_Id"].max().year - self.fullReservoirs["dato_Id"].min().year) * 12 + self.fullReservoirs["dato_Id"].max().month
+        self.months = self.minMonth
+
+    def plotData(self, column:str, months:int=1):
+        if column == "":
+            pass
+        elif column == "All columns":
+            reservoirs = reservoirsSelect(self.fullReservoirs, [], months)
+        else:
+            reservoirs = reservoirsSelect(self.fullReservoirs, [column], months)
+            fig = go.Figure(
+                data=[go.Histogram(x=reservoirs[column])]
+            )
+            fig.update_xaxes(title_text=column)
+            fig.update_yaxes(title_text="count")
+            fig.show()
+            st.plotly_chart(fig)
+
+    def renderPage(self):
+        # FillerShit
+        st.title("Nullam ac ornare tellus.")
+        st.write("Duis eget sollicitudin justo. Pellentesque aliquam congue turpis sed aliquet. Etiam vitae nulla non sem elementum vulputate. ")
+
+        # ActualStuff
+        optionsColumns = st.selectbox("Select what columns to display", self.fullColumns)
+        self.plotData(optionsColumns,months=months)
+        self.months = st.slider("Choose nr. of months to look at", self.minMonth, self.maxMonth)
+
+    def run(self):
+        self.renderPage()
+
+
+if __name__ == "__main":
+    try:
+        PlotData().run()
+    except Exception as e:
+        st.error(e)
--- pages/tabularData.py
+++ pages/tabularData.py
@@ -17,7 +17,7 @@ class TabularData:
             "first month": [self.reservoirsDF[columns].tolist() for columns in self.reservoirsDF.columns]
         })
 
-    def showData(self):
+    def showTable(self):
         tabularData = self.tabularData()
         st.dataframe(
             tabularData,
@@ -33,13 +33,14 @@ class TabularData:
             }
         )
 
+    # Render Page
     def renderPage(self):
         #dummy text
         st.title("Mauris lorem")
         st.write("elis, consectetur id mollis sit amet, vulputate non libero. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris eros purus, sagittis in finibus eu, ultricies sit amet nisi. Cras at sagittis eros.")
 
         #actual stuff
-        self.showData()
+        self.showTable()
 
     def run(self):
         self.renderPage()
