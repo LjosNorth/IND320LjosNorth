592ae2acedb2c4c58adaae6e658cf51d0cb10cd9
LjosNorth <hbborve@gmail.com>
2026-09-18 08:33:46 +0200

    Implemented userRoles

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,23 @@
 import streamlit as st
+from utils.UserRoles import UserRoles
+from utils.decorators import *
 
+#initiate role
+if "userRole" not in st.session_state:
+    st.session_state.userRole = UserRoles.VIEWER
+
+@require_role(UserRoles.VIEWER)
+def prage():
+    print("you can see what a viewer can, pog")
+prage()
+
+''' Pages '''
 pageHome = st.Page("pages/home.py", title="Home")
 pageTables = st.Page("pages/tabularData.py", title="Tables")
 pagePlots = st.Page("pages/plotData.py", title="Plots")
 pageSettings = st.Page("pages/page4.py", title="settings")
 
-if st.session_state.get("is_admit", True):
+if st.session_state.userRole == UserRoles.ADMIN:
     pages = [pageHome, pageTables, pagePlots, pageSettings]
 else:
     pages = [pageHome, pageTables, pagePlots]
---
+++ utils/decorators.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+from utils.UserRoles import UserRoles
+pass
+
+
+def require_role(required_role):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            userRole = st.session_state.userRole = UserRoles.ADMIN
+            if userRole.value >= required_role.value:
+                return func(*args, **kwargs)
+            else:
+                pass
+        return wrapper
+    return decorator
