ce5da0457ec249c5f10ba02d8d300efa4f71b486
LjosNorth <hbborve@gmail.com>
2026-09-18 12:09:25 +0200

    attempting to fix a rendering issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,20 +6,6 @@ from utils.decorators import *
 if "userRole" not in st.session_state:
     st.session_state.userRole = UserRoles.VIEWER
 
-#Pages
-pageHome = st.Page("pages/home.py", title="Home")
-pageTables = st.Page("pages/tabularData.py", title="Tables")
-pagePlots = st.Page("pages/plotData.py", title="Plots")
-pageSettings = st.Page("pages/page4.py", title="settings")
-
-if st.session_state.userRole == UserRoles.ADMIN:
-    pages = [pageHome, pageTables, pagePlots, pageSettings]
-else:
-    pages = [pageHome, pageTables, pagePlots]
-
-sidebar = st.navigation(pages, position="sidebar")
-sidebar.run()
-
 #Role Change Logic, for testing
 with st.sidebar:
     st.write("Change Role")
@@ -35,3 +21,17 @@ with st.sidebar:
         if st.button("VIEWER"):
             st.session_state.userRole = UserRoles.VIEWER
             st.rerun()
+
+#Pages
+pageHome = st.Page("pages/home.py", title="Home")
+pageTables = st.Page("pages/tabularData.py", title="Tables")
+pagePlots = st.Page("pages/plotData.py", title="Plots")
+pageSettings = st.Page("pages/page4.py", title="settings")
+
+if st.session_state.userRole == UserRoles.ADMIN:
+    pages = [pageHome, pageTables, pagePlots, pageSettings]
+else:
+    pages = [pageHome, pageTables, pagePlots]
+
+sidebar = st.navigation(pages, position="sidebar")
+sidebar.run()
