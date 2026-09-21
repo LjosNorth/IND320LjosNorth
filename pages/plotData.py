c75e33a1450de819e717d1e80cfb31e9f200a33c
LjosNorth <hbborve@gmail.com>
2026-09-21 20:15:08 +0200

    attempting to add All columns option to plotpage

--- pages/plotData.py
+++ pages/plotData.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.graph_objects as go
 
+from plotly.subplots import make_subplots
 from utils.reservoirsSelect import reservoirsSelect
 from utils.dataLoaders import load_Reservoirs
 
@@ -14,7 +15,7 @@ class PlotData:
 
         # Columns
         self.fullColumns=["dato_Id", "fyllingsgrad", "kapasitet_TWh", "fylling_TWh", "fyllingsgrad_forrige_uke", "endring_fyllingsgrad"]
-        # self.fullColumns.add("All Columns")
+        self.fullColumns.append("All Columns")
 
         # Months
         self.minMonth = self.fullReservoirs["dato_Id"].min().month
@@ -25,6 +26,7 @@ class PlotData:
             pass
         elif column == "All columns":
             reservoirs = reservoirsSelect(self.fullReservoirs, [], months)
+            self.figureAllColumns(reservoirs)
         else:
             reservoirs = reservoirsSelect(self.fullReservoirs, [column], months)
             fig = go.Figure(
@@ -34,6 +36,53 @@ class PlotData:
             fig.update_yaxes(title_text="count")
             fig.show()
             st.plotly_chart(fig)
+
+    def figureAllColumns(self, reservoirs:pd.DataFrame):
+        fig = make_subplots(rows=3, cols=2)
+        fig.add_trace(
+            go.Histogram(x=reservoirs["fyllingsgrad"], name="fyllingsgrad"),
+            row=1, col=1
+        )
+        fig.add_trace(
+            go.Histogram(x=reservoirs["fylling_TWh"], name="fylling_TWh"),
+            row=1, col=2
+        )
+        fig.add_trace(
+            go.Histogram(x=reservoirs["fyllingsgrad_forrige_uke"], name="fyllingsgrad_forrige_uke"),
+            row=2, col=1
+        )
+        fig.add_trace(
+            go.Histogram(x=reservoirs["endring_fyllingsgrad"], name="endring_fyllingsgrad"),
+            row=2, col=2
+        )
+        fig.add_trace(
+            go.Histogram(x=reservoirs["kapasitet_TWh"], name="kapasitet_TWh"),
+            row=3, col=1
+        )
+
+        fig.update_layout(title_text="Reservoirs", showlegend=True)
+
+        # figure1
+        fig.update_xaxes(title="fyllingsgrad", row=1, col=1)
+        fig.update_yaxes(title="count", row=1, col=1)
+
+        # figure2
+        fig.update_xaxes(title="fylling_TWh", row=1, col=2)
+        fig.update_yaxes(title="count", row=1, col=2)
+
+        # figure3
+        fig.update_xaxes(title="fyllingsgrad_forrige_uke", row=2, col=1)
+        fig.update_yaxes(title="count", row=2, col=1)
+
+        # figure4
+        fig.update_xaxes(title="endring_fyllingsgrad", row=2, col=2)
+        fig.update_yaxes(title="count", row=2, col=2)
+
+        # figure5
+        fig.update_xaxes(title="kapasitet_TWh", row=3, col=1)
+        fig.update_yaxes(title="count", row=3, col=1)
+
+        st.plotly_chart(fig, use_container_width=True)
 
     def renderPage(self):
         # FillerShit
